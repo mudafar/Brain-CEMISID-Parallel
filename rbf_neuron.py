96a0f278a0d73cd1ec3291ace1b2aea5780c6667
breytner <breytnerm@gmail.com>
2016-07-10 13:17:54 -0400

    PEP-8 methods naming compliance

--- rbf_neuron.py
+++ rbf_neuron.py
@@ -3,7 +3,7 @@ from math import fabs
 class RbfNeuron:
     """ Radial Base Function neuron class """
     # Number of class instances
-    nNum = 0
+    instances_count = 0
     # Size of data or knowledge in bytes
     DATA_SIZE = 4.0
     # Default radius
@@ -12,64 +12,64 @@ class RbfNeuron:
     # Class constructor
     def __init__(self):
         # Neuron has no knowledge when created
-        self.setHasKnowledge(False)
+        self.set_has_knowledge(False)
         # Neuron has default radius when created
-        self.setRadius(RbfNeuron.DEFAULT_RADIUS)
+        self.set_radius(RbfNeuron.DEFAULT_RADIUS)
         # Increment number of class instances
-        RbfNeuron.nNum += 1
+        RbfNeuron.instances_count += 1
         
     """ Returns whether neuron has knowledge or not """
-    def hasKnowledge(self):
-        return self.hasKnowl
+    def has_knowledge(self):
+        return self.has_knowledge
     
-    """ Sets the has-knowledge (hasKnowl) internal flag """
-    def setHasKnowledge(self, value ):
-        self.hasKnowl = value
+    """ Sets the has-knowledge (has_knowledge) internal flag """
+    def set_has_knowledge(self, value ):
+        self.has_knowledge = value
         
     """ Returns wheter neuron is memeber of set """
-    def isMember(self, testSet):
-        return (self.nSet == testSet)
+    def is_member(self, test_set):
+        return (self.neuron_set == test_set)
         
     """ Sets neuron radius """
-    def setRadius(self, nRadius):
-        self.radius = nRadius
+    def set_radius(self, new_radius):
+        self.radius = new_radius
         
     """ Gets neuron radius """
-    def getRadius(self):
+    def get_radius(self):
         return self.radius
     
     """ Sets neuron class """
-    def setClass(self, nClass ):
-        self.nClass = nClass
+    def set_class(self, new_class ):
+        self.neuron_class = new_class
         
     """ Gets neuron class """
-    def getClass(self):
-        if self.nClass == None:
+    def get_class(self):
+        if self.neuron_class == None:
             raise ValueError
-        return self.nClass
+        return self.neuron_class
 
     """ Sets neuron set """
-    def setSet(self, nSet ):
-        self.nSet = nSet
+    def set_set(self, new_set ):
+        self.neuron_set = new_set
         
     """ Gets neuron set """
-    def getSet(self):
-        if self.nSet == None:
+    def get_set(self):
+        if self.neuron_set == None:
             raise ValueError
-        return self.nSet
+        return self.neuron_set
         
     """ Sets neuron data or 'center' of knowledge """
-    def setData(self, data):
-        self.data = data
+    def set_data(self, new_data):
+        self.neuron_data = new_data
         
     """ Gets neuron data or 'center' of knowledge """
-    def getData(self, data):
-        if self.data == None:
+    def get_data(self, new_data):
+        if self.neuron_data == None:
             raise ValueError
-        return self.data
+        return self.neuron_data
         
     """ Calculates Manhattan distance, private method """
-    def _calcManhattanDist(self, pattern):
+    def _calculate_manhattan_distance(self, pattern):
     	# Given pattern must be of size equal to DATA_SIZE
         if len(pattern) != RbfNeuron.DATA_SIZE:
             raise ValueError
@@ -77,17 +77,19 @@ class RbfNeuron:
         self.distance = 0
         # Calculate Manhattan distance
         for index in range(len(pattern)):
-            self.distance += fabs(self.data[index]-pattern[index])
+            self.distance += fabs(self.neuron_data[index]-pattern[index])
+        # Return distance
         return self.distance
     
     """ Returns true if neuron recognizes given pattern """
     def recognize(self, pattern ):
     	# If Manhattan distance to pattern is less than neuron radius
     	# there is a hit
-        if self._calcManhattanDist(pattern) < self.radius:
+        if self._calculate_manhattan_distance(pattern) < self.get_radius():
             self.hit = True
         else:
             self.hit = False
+        # Return whether there has been a hit or not
         return self.hit
     
     """ Learns a new pattern (knowledge, data), return true if
@@ -95,29 +97,30 @@ class RbfNeuron:
     def learn(self, pattern, class_, set_):
         # Tries to store pattern
         try:
-            self.setData(pattern)
+            self.set_data(pattern)
         except TypeError:
-            print "pattern_ must be a list of integers"
+            print "pattern must be a list of integers"
             return False
-        
         # Tries to store pattern class 
         try:
-            self.setClass(class_)
+            self.set_class(class_)
         except TypeError:
             print "class_ must be a string"
             return False
             
         # Tries to store class set
         try:
-             self.setSet(set_)
+             self.set_set(set_)
         except TypeError:
             print "set_ must be a string"
-            return False
-            
+            return False    
         # Indicate that this neuron has knowledge
-        self.setHasKnowledge(True)
+        self.set_has_knowledge(True)
+        # Return True to indicate proper learning process
+        return True
+
     """ Reduces neuron radius by 'value' """
-    def reduceRadiusBy(self, value ):
+    def reduce_radius_by(self, value ):
         if value < 0 :
             raise ValueError("value must be positive")
         if self.radius < value :
--- tests.py
+++ tests.py
@@ -11,22 +11,22 @@ if __name__ == '__main__':
     
     # States whether the pattern can be recognized
     if n1.recognize([0.9,0.8,1,1]):
-        print "I can recognize a " + n1.getClass() + ", which is a kind of " + n1.getSet()
+        print "I can recognize a " + n1.get_class() + ", which is a kind of " + n1.get_set()
     else:
         print "I can't recognize the given pattern"
     
     # States whether the pattern can be recognized
     if n1.recognize([0.9,0.8,1,0.9]):
-        print "I can recognize a " + n1.getClass() + ", which is a kind of " + n1.getSet()
+        print "I can recognize a " + n1.get_class() + ", which is a kind of " + n1.get_set()
     else:
         print "I can't recognize the given pattern"
     
     # Reduces neuron radio
-    n1.reduceRadiusBy(0.3)
+    n1.reduce_radius_by(0.3)
     
     # States whether the pattern can be recognized
     if n1.recognize([0.9,0.8,1,0.9]):
-        print "I can recognize a " + n1.getClass() + ", which is a kind of " + n1.getSet()
+        print "I can recognize a " + n1.get_class() + ", which is a kind of " + n1.get_set()
     else:
         print "I can't recognize the given pattern"
         
